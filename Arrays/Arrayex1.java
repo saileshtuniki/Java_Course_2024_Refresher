48b47525771cc6a44c572670d650fa790c6b151a
saileshtuniki <sailesh.tuniki@gmail.com>
2024-08-13 19:20:39 +0530

    ArrayConcept

---
+++ Arrays/Arrayex1.java
@@ -0,0 +1,12 @@
+public class Arrayex1 {
+    public static void main(String[] args) {
+        
+        int nums[] = {3,4,5,6};
+
+        nums[2] = 9; // in the index position of 2 there is val 5 , we get 9 
+
+        System.out.println(nums[1]);  //4
+        System.out.println(nums[2]);  //9
+
+    }
+}
---
+++ Arrays/Arrayex2.java
@@ -0,0 +1,19 @@
+// package Arrays;
+
+public class Arrayex2 {
+    public static void main(String[] args) {
+
+        //Dynamic
+
+        int nums[] =  new int[4];
+
+        nums[0] = 4 ;
+        nums[1] = 8 ;
+        nums[2] = 3 ;
+        nums[3] = 9 ;
+
+        for (int i=0;i<4;i++){
+            System.out.println(nums[i]);
+        }
+    }
+}
